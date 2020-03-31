40257d99643bcdab6b0956877ae13bd52e90c59a
gaoh <5146513+gaohwh@user.noreply.gitee.com>
2020-03-31 21:43:55 +0800

    Java1.8源码 干掉了com.sun下面的源码

---
+++ src/cn/gaoh/Main.java
@@ -0,0 +1,17 @@
+package cn.gaoh;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    //GTKStyle com.sun.java.swing.plaf.gtk.GTKStyle
+    public static void main(String[] args) {
+	// write your code here
+        Map<String,Integer> map = new HashMap<>(6);
+        map.put("1", 1);
+        map.put("2", 2);
+        map.put("3", 4);
+        map.put("4", 4);
+    }
+}
